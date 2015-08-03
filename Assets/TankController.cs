9cb84345e6afaafb51535b25dc2393fe69021673
Neron94 <neron_venom@Mail.ru>
2015-08-03 09:12:57 +0500

    Работа над Танк контроллерам

--- Assets/TankController.cs
+++ Assets/TankController.cs
@@ -9,30 +9,33 @@ public class TankController : MonoBehaviour {
     public int turnSpeed = 30;
     public Vector3 moveTar;
     public GameObject tower;
-    public Vector3 targMouse; 
+    public Vector3 targMouse;
+   
    
 
     
 	
 	void Update () {
 
-        float y = Input.mousePosition.y;
-        targMouse = new Vector3(0, y, 0);
+
+
+
+        float Yaxis = Input.mousePosition.y;
+        targMouse.y = Yaxis;
+        
+
+        if (tower.transform.rotation.y > Yaxis)
+        {
+            tower.transform.rotation = Quaternion.Lerp(transform.rotation, targMouse, turnSpeed * Time.deltaTime);
+        }
+        if (tower.transform.rotation.y < Yaxis)
+        {
+            tower.transform.Rotate(-Vector3.up * turnSpeed * Time.deltaTime);
+        }
+       
 
             
-
-            if (true)
-            {
-                tower.transform.LookAt(targMouse);
-            }
-            else { }
-
-        
-        
-        
-        
-        
-        
+ 
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward *speed* Time.deltaTime);
